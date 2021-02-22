dfb07b20f27df10cb84e0b022226e23903717938
ealtun <Ealtun1989>
2021-02-22 18:10:24 +0300

    first commit

---
+++ config-server/src/test/java/tr/com/innova/legalite/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.innova.legalite.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
